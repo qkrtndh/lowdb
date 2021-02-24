41c5e0947e186c453cb78190d46b0f3887dd01a0
qkrtndh <qkrtndh5@naver.com>
2021-02-24 17:16:58 +0900

    웹/nodejs lowdb 설치

---
+++ main.js
@@ -0,0 +1,19 @@
+const low = require('lowdb') //lowdb 모듈을 불러온다
+const FileSync = require('lowdb/adapters/FileSync')//어떤방식으로 데이터를 저장할 것인가. 여기서는 파일동기형식
+const adapter = new FileSync('db.json')//데이터를 db.json이라는 파일에 json 형식으로 저장하겠다.
+const db = low(adapter)//lowdb에 파일 동기형식으로 데이터를 저장하겠다고 지정.
+var http = require('http');	// 서버 만드는 모듈 불러오기
+var fs = require('fs');
+var app = http.createServer(function(request,response){
+    var url = request.url;
+    if(request.url == '/'){
+      url = '/index.html';	// 실행할 url
+    }
+    if(request.url == '/favicon.ico'){
+      return response.writeHead(404);
+    }
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + url));
+ 
+});
+app.listen(3000);
