f7dc0b096630460e411cd50c3a76d1079a2a6720
qkrtndh <qkrtndh5@naver.com>
2021-02-24 17:28:51 +0900

    db.json 기본 초기화

--- main.js
+++ main.js
@@ -2,6 +2,16 @@ const low = require('lowdb') //lowdb 모듈을 불러온다
 const FileSync = require('lowdb/adapters/FileSync')//어떤방식으로 데이터를 저장할 것인가. 여기서는 파일동기형식
 const adapter = new FileSync('db.json')//데이터를 db.json이라는 파일에 json 형식으로 저장하겠다.
 const db = low(adapter)//lowdb에 파일 동기형식으로 데이터를 저장하겠다고 지정.
+
+db.defaults({topic:[],auth:[]}).write();//기본적으로 준비되어야 할 데이터를 초기화한다. 객체형식으로 json 파일에 추가된다.
+
+
+
+
+
+
+
+/*
 var http = require('http');	// 서버 만드는 모듈 불러오기
 var fs = require('fs');
 var app = http.createServer(function(request,response){
@@ -16,4 +26,4 @@ var app = http.createServer(function(request,response){
     response.end(fs.readFileSync(__dirname + url));
  
 });
-app.listen(3000);
+app.listen(3000);*/
